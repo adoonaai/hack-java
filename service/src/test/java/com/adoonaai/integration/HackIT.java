8017b5e33e3758dfe8a053186e7e8a8373893293
andrey.korytin <themarkling@gmail.com>
2022-03-28 18:17:24 +0300

    create apache maven project

---
+++ service/src/test/java/com/adoonaai/integration/HackIT.java
@@ -0,0 +1,16 @@
+package com.adoonaai.integration;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HackIT {
+
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
+
